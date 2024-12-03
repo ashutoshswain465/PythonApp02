2f25b6f7d62e46b63ef8d1526edfb683c58436c0
ASHUTOSH SWAIN <ashutoshswain465@gmail.com>
2024-12-03 20:39:52 +0530

    Send Email using forms

--- send_email.py
+++ send_email.py
@@ -1,22 +1,15 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "ashutoshswain465@gmail.com"
+    password = "qspe knfl kufx wtbc"
+    receiver = "ashutoshswain465@gmail.com"
+    context = ssl.create_default_context()
 
-username = "ashutoshswain465@gmail.com"
-password = "qspe knfl kufx wtbc"
-
-receiver = "ashutoshswain465@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Email test for python webforms
-Hi!
-Testing email sent successfully.
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
